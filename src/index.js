5465ea3b0c11b2dc7ef3f944574090fbbf54d4b5
sumitdeve <sumitdev9670@gmail.com>
2024-06-02 10:52:40 +0530

    error handler

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
-import express from 'express'
-import { connectDB } from './db/index.js'
+import { connectDB } from "./db/index.js";
+import { app } from "./app.js";
 
-
-const app = express()
-
-const PORT = 8000
-
-
-app.use(express.json())
+const PORT = 8000;
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${PORT}`);
+    });
+  })
+  .catch((err) => console.log("MONGO db connection failed !!!", err));
 
-
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
-
-
-
-
+// app.listen(PORT,()=>{
+//     console.log(`Server is running on port ${PORT}`)
+// })
